3fddfbf4ee39df191a59db53392ff4a1e1677b22
Paarisena <paarisena@hotmail.com>
2025-10-07 00:50:46 +0530

    Commit

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 8000;
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", "https://avgallery.shop"],
+        origin: ["http://localhost:5173", "https://avgallery.shop","https://www.avgallery.shop"],
         methods: ["GET", "POST"],
         credentials: true
     }
@@ -32,7 +32,8 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 app.use(express.static(path.join(__dirname,"uploads")))
-app.use(cors( {origin: ["http://localhost:5173", "https://avgallery.shop"],credentials: true}))
+app.use(cors( {origin: ["http://localhost:5173", "https://avgallery.shop",
+"https://www.avgallery.shop"],credentials: true}))
 app.use('/api/payments/webhook', express.raw({type: 'application/json'}))
 app.use(express.json(({ limit: '40mb' })))
 
