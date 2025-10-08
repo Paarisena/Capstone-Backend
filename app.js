787ee3caf7c924ff9de18c71f9baab059e1528b6
Paarisena <paarisena@hotmail.com>
2025-10-08 22:56:05 +0530

    Update

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 8000;
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", "https://avgallery.shop","https://www.avgallery.shop"],
+        origin: ["http://localhost:5173", "https://avgallery.shop","https://www.avgallery.shop","https://avgallery.netlify.app"],
         methods: ["GET", "POST"],
         credentials: true
     }
@@ -33,7 +33,7 @@ const __dirname = path.dirname(__filename)
 
 app.use(express.static(path.join(__dirname,"uploads")))
 app.use(cors( {origin: ["http://localhost:5173", "https://avgallery.shop",
-"https://www.avgallery.shop"],credentials: true}))
+"https://www.avgallery.shop","https://avgallery.netlify.app"],credentials: true}))
 app.use('/api/payments/webhook', express.raw({type: 'application/json'}))
 app.use(express.json(({ limit: '40mb' })))
 
