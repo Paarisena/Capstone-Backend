7fc81326f1b2c259491aeaa3cea78efa2735539e
Paarisena <paarisena@hotmail.com>
2025-10-17 20:36:17 +0530

    Security Fix and direct Purchase

--- Routes/ProductRoutes.js
+++ Routes/ProductRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import {addProduct,listProducts,deleteProduct, listPublicProducts, editProduct, addProfile, fetchProfile, sendEmail} from '../Login page/Dashboard.js';
 import upload from '../Middleware/Multer.js';
 import adminAuth from '../Middleware/adminAuth.js';
-import { addToCart, updateCart,getUserCart, deleteFromCart } from '../Login page/CartModel.js';
+import { addToCart, updateCart,getUserCart, deleteFromCart, directPurchase, getUserOrders } from '../Login page/CartModel.js';
 
 const ProductRouter = express.Router();
 
@@ -10,13 +10,15 @@ ProductRouter.post('/addProducts',adminAuth,upload.fields([{name:'image1', maxCo
 ProductRouter.get('/products',adminAuth, listProducts);
 ProductRouter.put('/edit/:id',adminAuth, editProduct);
 ProductRouter.get('/public-products', listPublicProducts);
-ProductRouter.post('/profile',addProfile);
-ProductRouter.get('/userProfile',fetchProfile);
+ProductRouter.post('/profile',adminAuth,addProfile);
+ProductRouter.get('/userProfile',adminAuth,fetchProfile);
 ProductRouter.delete('/delete/:id',adminAuth, deleteProduct);
 ProductRouter.post('/cart/add', addToCart);
 ProductRouter.put('/cart/update/:itemId', updateCart);
 ProductRouter.get('/cart', getUserCart);
 ProductRouter.delete('/cart/delete/:itemId', deleteFromCart);
+ProductRouter.post('/direct-purchase', directPurchase);
+ProductRouter.get('/user-orders', getUserOrders);
 ProductRouter.post('/send-email', sendEmail);
 
 
