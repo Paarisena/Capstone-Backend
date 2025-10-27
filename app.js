52922e05c73657de41fed513111849045a79d9c5
Paarisena <paarisena@hotmail.com>
2025-10-27 17:10:55 +0530

    Commit

--- app.js
+++ app.js
@@ -49,7 +49,15 @@ const PORT = process.env.PORT || process.env.WEBSITES_PORT || 8000
 
 // Configure CORS early
 app.use(cors({
-    origin: allowedOrigins,
+    origin: function(origin, callback) {
+        // allow requests with no origin (like mobile apps or curl requests)
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+            var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: [
         'Content-Type',
@@ -65,9 +73,6 @@ app.use(cors({
 }));
 
 app.use(cookieParser());
-
-// Add preflight handler
-app.options('*', cors());
 
 
 
